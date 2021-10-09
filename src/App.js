8d321911232ec58f64e86e599069e2ae8d604680
AlehBarushka <1.AlehBarushka@gmail.com>
2021-10-09 14:15:24 +0300

    added grid styles

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="wrapper">
+    <div className="app-wrapper">
       <header className="header">
         <img
-          src="https://upload.wikimedia.org/wikipedia/commons/4/4a/100x100_logo.png"
-          alt=""
+          src="https://cdn.dribbble.com/users/24078/screenshots/15522433/media/e92e58ec9d338a234945ae3d3ffd5be3.jpg?compress=1&resize=400x300"
+          alt="logo"
         />
       </header>
       <nav className="nav">
         <div>
-          <a href="">Home</a>
+          <a href="/#">Profile</a>
         </div>
         <div>
-          <a href="">About</a>
+          <a href="/#">Messages</a>
         </div>
         <div>
-          <a href="">Messages</a>
+          <a href="/#">News</a>
         </div>
         <div>
-          <a href="">Music</a>
+          <a href="/#">Music</a>
         </div>
         <div>
-          <a href="">Settings</a>
+          <a href="/#">Settings</a>
         </div>
       </nav>
       <div className="content">
-        <img src="https://picsum.photos/id/1/1000/600" alt="" />
+        <img src="https://picsum.photos/id/1/1200/500" alt="main img" />
+        <div>ava + description</div>
+        <div>
+          My posts
+          <div>New post</div>
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
       </div>
     </div>
   );
