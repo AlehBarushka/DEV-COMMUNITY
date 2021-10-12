034fa3c4cb6cb2e6ab8369cebd02894bf5149a89
AlehBarushka <1.alehbarushka@gmail.com>
2021-10-12 16:25:07 +0300

    create submodules

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,20 @@
+import styles from './MyPosts.module.css';
+
+const Profile = () => {
+  return (
+    <div className={styles.content}>
+      <img src="https://picsum.photos/id/1/1200/500" alt="main img" />
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div className={styles.posts}>
+          <div className={styles.item}>post 1</div>
+          <div className={styles.item}>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
