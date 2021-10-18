4411f31fb43a560d6f97e66e2c99ad84fbfdd3ec
AlehBarushka <1.alehbarushka@gmail.com>
2021-10-18 11:45:46 +0300

    added mesages, navlinks and stylws fo them

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,38 @@
-import styles from "./Dialogs.module.css";
+import { NavLink } from 'react-router-dom';
+import styles from './Dialogs.module.css';
 
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={styles.dialogs}>
+      <div className={styles['dialogs-items']}>
+        <div className={styles.dialog}>
+          <NavLink to="/dialog1" activeClassName={styles.active}>
+            Dialog1
+          </NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialog2" activeClassName={styles.active}>
+            Dialog2
+          </NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialog3" activeClassName={styles.active}>
+            Dialog3
+          </NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialog4" activeClassName={styles.active}>
+            Dialog4
+          </NavLink>
+        </div>
+      </div>
+      <div className={styles.messages}>
+        <div className={styles.message}>Hi!</div>
+        <div className={styles.message}>How are you?</div>
+        <div className={styles.message}>Yo)))</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
