1cb566424f871d1b91f97bd4ae003968c4dae0af
AlehBarushka <1.AlehBarushka@gmail.com>
2022-01-11 15:05:05 +0300

    transferring a request to componentDidMount

--- react-app-old/src/components/Users/UsersClassComponent.jsx
+++ react-app-old/src/components/Users/UsersClassComponent.jsx
@@ -5,8 +5,7 @@ import * as axios from 'axios';
 import unknowUser from '../../assets/img/unknowUser.jpg';
 
 class UsersClassComponent extends Component {
-	constructor(props) {
-		super(props);
+	componentDidMount() {
 		axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
 			this.props.setUsers(response.data.items);
 		});
