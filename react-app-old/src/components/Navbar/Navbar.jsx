02c6afdf6e6400a9e29327785edabd915e408cba
AlehBarushka <1.AlehBarushka@gmail.com>
2022-01-11 14:44:37 +0300

    added users

--- react-app-old/src/components/Navbar/Navbar.jsx
+++ react-app-old/src/components/Navbar/Navbar.jsx
@@ -2,35 +2,35 @@ import { NavLink } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
-  return (
-    <nav className={styles.nav}>
-      <div className={styles.item}>
-        <NavLink to="/profile" activeClassName={styles.active}>
-          Profile
-        </NavLink>
-      </div>
-      <div className={styles.item}>
-        <NavLink to="/dialogs" activeClassName={styles.active}>
-          Messages
-        </NavLink>
-      </div>
-      <div className={styles.item}>
-        <NavLink to="/news" activeClassName={styles.active}>
-          News
-        </NavLink>
-      </div>
-      <div className={styles.item}>
-        <NavLink to="/music" activeClassName={styles.active}>
-          Music
-        </NavLink>
-      </div>
-      <div className={styles.item}>
-        <NavLink to="/settings" activeClassName={styles.active}>
-          Settings
-        </NavLink>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className={styles.nav}>
+			<div className={styles.item}>
+				<NavLink to='/profile' activeClassName={styles.active}>
+					Profile
+				</NavLink>
+			</div>
+			<div className={styles.item}>
+				<NavLink to='/dialogs' activeClassName={styles.active}>
+					Messages
+				</NavLink>
+			</div>
+			<div className={styles.item}>
+				<NavLink to='/users' activeClassName={styles.active}>
+					Users
+				</NavLink>
+			</div>
+			<div className={styles.item}>
+				<NavLink to='/music' activeClassName={styles.active}>
+					Music
+				</NavLink>
+			</div>
+			<div className={styles.item}>
+				<NavLink to='/settings' activeClassName={styles.active}>
+					Settings
+				</NavLink>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
