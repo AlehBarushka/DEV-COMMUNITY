ca53b818a78fe191b61d58165d732df7b5ad7249
AlehBarushka <1.AlehBarushka@gmail.com>
2022-01-11 13:29:42 +0300

    bypassing side effects

--- react-app-old/src/components/Users/Users.jsx
+++ react-app-old/src/components/Users/Users.jsx
@@ -5,14 +5,17 @@ import * as axios from 'axios';
 import unknowUser from '../../assets/img/unknowUser.jpg';
 
 const Users = (props) => {
-	if (props.users.length === 0) {
-		axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
-			props.setUsers(response.data.items);
-		});
-	}
+	const getUsers = () => {
+		if (props.users.length === 0) {
+			axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
+				props.setUsers(response.data.items);
+			});
+		}
+	};
 
 	return (
 		<div>
+			<button onClick={getUsers}>Get users</button>
 			{props.users.map((user) => (
 				<div key={user.id}>
 					<div className={styles.ava}>
