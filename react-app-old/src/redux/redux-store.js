598e21bc3be7928e2f5d6cc0a9b38a4b8a24e3e3
AlehBarushka <1.AlehBarushka@gmail.com>
2022-01-21 14:07:16 +0300

    added thunk to store

--- react-app-old/src/redux/redux-store.js
+++ react-app-old/src/redux/redux-store.js
@@ -1,4 +1,6 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
 import profileReducer from './profile-reducer';
 import dialogsReducer from './dialogs-reducer';
 import usersReducer from './users-reducer';
@@ -11,7 +13,7 @@ let reducers = combineReducers({
 	auth: authReducer,
 });
 
-let store = createStore(reducers);
+let store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 window.store = store;
 
