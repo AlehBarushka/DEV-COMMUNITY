94553f15943f7bb753b2d36238acb2cc9bee170f
AlehBarushka <1.AlehBarushka@gmail.com>
2022-01-23 23:08:05 +0300

    added get and update status

--- react-app-old/src/redux/profile-reducer.js
+++ react-app-old/src/redux/profile-reducer.js
@@ -1,8 +1,9 @@
-import { usersAPI } from '../api/api';
+import { profileAPI, usersAPI } from '../api/api';
 
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
+const SET_STATUS = 'SET_STATUS';
 
 let initialState = {
 	postsData: [
@@ -19,6 +20,7 @@ let initialState = {
 	],
 	newPostText: '',
 	profile: null,
+	status: '',
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -46,6 +48,10 @@ const profileReducer = (state = initialState, action) => {
 			stateCopy = { ...state, profile: action.profile };
 			return stateCopy;
 
+		case SET_STATUS:
+			stateCopy = { ...state, status: action.status };
+			return stateCopy;
+
 		default:
 			return state;
 	}
@@ -60,10 +66,33 @@ export const updateNewPostTextAC = (text) => ({
 
 export const setUserProfileAC = (profile) => ({ type: SET_USER_PROFILE, profile: profile });
 
+export const setStatusAC = (status) => ({
+	type: SET_STATUS,
+	status: status,
+});
+
 export const getUserProfileThunkCreator = (userId) => {
 	return (dispatch) => {
 		usersAPI.getProfile(userId).then((response) => {
 			dispatch(setUserProfileAC(response));
+		});
+	};
+};
+
+export const getUserStatusThunkCreator = (userId) => {
+	return (dispatch) => {
+		profileAPI.getStatus(userId).then((response) => {
+			dispatch(setStatusAC(response));
+		});
+	};
+};
+
+export const updateUserStatusThunkCreator = (status) => {
+	return (dispatch) => {
+		profileAPI.updateStatus(status).then((response) => {
+			if (response.data.resultCode === 0) {
+				dispatch(setStatusAC(status));
+			}
 		});
 	};
 };
