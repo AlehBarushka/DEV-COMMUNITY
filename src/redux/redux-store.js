32b7fd07c160bfe9ea9378286c85f4d8bf63d40c
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-12 15:25:37 +0300

    add dev tools

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,10 +1,10 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 
 import profileReducer from './profile-reducer';
 import dialogsReducer from './dialogs-reducer';
 import usersReducer from './users-reducer';
 import authReducer from './auth-reducer';
+import thunk from 'redux-thunk';
 
 let reducers = combineReducers({
 	profilePage: profileReducer,
@@ -12,8 +12,9 @@ let reducers = combineReducers({
 	usersPage: usersReducer,
 	auth: authReducer,
 });
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+let store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 window.store = store;
 
