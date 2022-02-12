94f065af02a9b99d87192a3ab77c8c56fef59747
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-12 15:26:20 +0300

    del withRouter hoc

--- src/hoc/withRouter.js
+++
@@ -1,9 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-const withRouter = (WrappedComponent) => (props) => {
-	const params = useParams();
-
-	return <WrappedComponent {...props} params={params} />;
-};
-
-export default withRouter;
