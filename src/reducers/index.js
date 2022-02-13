771e1a57ec6ce4a3f2e5e69c716d639f10997718
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-13 23:32:19 +0300

    some refactoring redux file strucure

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+
+import profileReducer from './profile-reducer';
+import dialogsReducer from './dialogs-reducer';
+import usersReducer from './users-reducer';
+import authReducer from './auth-reducer';
+
+const rootReducers = combineReducers({
+	profilePage: profileReducer,
+	dialogsPage: dialogsReducer,
+	usersPage: usersReducer,
+	auth: authReducer,
+});
+
+export default rootReducers;
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducers from '../reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducers, composeEnhancers(applyMiddleware(thunk)));
+
+window.store = store;
+
+export default store;
