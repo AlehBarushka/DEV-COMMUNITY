9ac974977546e0762f7906d37a327bb80822d945
AlehBarushka <1.AlehBarushka@gmail.com>
2022-02-13 23:34:53 +0300

    del unnecessary components

--- src/components/News/News.jsx
+++
@@ -1,7 +0,0 @@
-// import styles from "./News.module.css";
-
-const News = (props) => {
-  return <div>News</div>;
-};
-
-export default News;
--- src/components/Settings/Settings.jsx
+++
@@ -1,7 +0,0 @@
-// import styles from "./Settings.module.css";
-
-const Settings = (props) => {
-  return <div>Settings</div>;
-};
-
-export default Settings;
